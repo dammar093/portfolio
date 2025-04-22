87a56684aecf0ef760ca687f71aeabe2117b54d6
dammar093 <dammarrana093@gmail.com>
2025-04-22 10:02:30 +0545

    added meta desctiption

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -6,6 +6,11 @@ import React from "react";
 const Projects = () => {
   return (
     <section className="px-2 md:px-20 w-full h-full flex flex-col gap-5">
+      <meta
+        name="description"
+        content="Explore projects showcasing my work and skills. Visit my GitHub for more details."
+      />
+      <title>Projects - Portfolio</title>
       <h1 className="text-2xl font-bold text-gray-800 dark:text-gray-200">
         Projects
       </h1>
