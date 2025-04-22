9e851344a25c2e5f6c25da81d3b26d489fb3233e
dammar093 <dammarrana093@gmail.com>
2025-04-22 20:38:35 +0545

    add text color in moon icon

--- src/components/theme/Mode.tsx
+++ src/components/theme/Mode.tsx
@@ -20,7 +20,7 @@ export function ModeToggle() {
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon">
           <Sun className="text-teal-500 dark:text-teal-500 h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <Moon className="text-teal-500 dark:text-teal-500 absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
